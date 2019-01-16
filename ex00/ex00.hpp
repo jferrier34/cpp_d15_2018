f03cf60b60abef60f6f0c2794bcc66ed8239d2ff
Julien Ferrier <jferrier@localhost.localdomain>
2019-01-16 11:08:35 +0100

    [added] the exercise 00 is the easiest exercise of this cpool

---
+++ ex00/ex00.hpp
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2019
+** Picture.hpp
+** File description:
+** ex02
+*/
+
+#ifndef _EX01_HPP
+#define _EX01_HPP
+
+#include <iostream>
+#include <string>
+
+template<typename T>
+void swap(T &a, T &b)
+{
+        int tmp;
+        tmp = a;
+        a = b;
+        b = tmp;
+}
+
+template<typename T>
+T min(T &a, T &b)
+{
+        if (a < b)
+                return a;
+        if (a > b) 
+                return b;
+        else
+                return b;
+}
+
+template<typename T>
+T max(T &a, T &b)
+{
+        if (a > b)
+                return (a);
+        if (a < b)
+                return (b);
+        else
+                return b;
+}
+
+template<typename T>
+T add(T &a, T &b)
+{
+        int result;
+        result = a + b;
+        return result;
+}
+
+#endif
